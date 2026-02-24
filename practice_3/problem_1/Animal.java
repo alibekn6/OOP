c464ebc7ac8c395d0c0bf29e317f52c05b89759f
alibekn6 <alibekandanuarbek@gmail.com>
2026-02-24 09:32:21 +0500

    practice 3

---
+++ practice_3/problem_1/Animal.java
@@ -0,0 +1,7 @@
+package problem1;
+
+class Animal {
+    void eat() {
+        System.out.println("Animal is eating!");
+    }
+}
---
+++ practice_3/problem_1/Dog.java
@@ -0,0 +1,9 @@
+package problem1;
+
+class Dog extends Animal {
+    void bark() {
+        System.out.println("im barking!");
+    }
+}
+
+
